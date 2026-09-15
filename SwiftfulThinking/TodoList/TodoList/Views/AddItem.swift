b630ac71977d47e30770d176d4422620fe780d58
areekaras <shibili.areekara@gmail.com>
2026-09-15 12:18:18 +0530

    Add views

---
+++ SwiftfulThinking/TodoList/TodoList/Views/AddItem.swift
@@ -0,0 +1,44 @@
+//
+//  AddItem.swift
+//  TodoList
+//
+//  Created by Shibili Areekara on 15/09/26.
+//
+
+import SwiftUI
+
+struct AddItem: View {
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
+                    .cornerRadius(10)
+                
+                Button {
+                    
+                } label: {
+                    Text("Save")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                }
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add item 🖊️")
+    }
+}
+
+#Preview {
+    NavigationView {
+        AddItem()
+    }
+}

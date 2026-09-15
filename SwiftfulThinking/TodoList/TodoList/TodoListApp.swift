651b67044602b5924dd90c01bcffb7f0b819f98e
areekaras <shibili.areekara@gmail.com>
2026-09-15 11:04:44 +0530

    Initial commit - Todo list project

---
+++ SwiftfulThinking/TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,19 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Shibili Areekara on 15/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ListView()
+            }
+        }
+    }
+}

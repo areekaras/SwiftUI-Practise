f1f4a6af00aef69de623d35b8d9342a5f3cca674
areekaras <shibili.areekara@gmail.com>
2026-09-15 22:19:53 +0530

    Add a view model with Environment Object

--- SwiftfulThinking/TodoList/TodoList/Views/AddItem.swift
+++ SwiftfulThinking/TodoList/TodoList/Views/AddItem.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct AddItem: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -19,9 +25,7 @@ struct AddItem: View {
                     .background(Color(#colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)))
                     .cornerRadius(10)
                 
-                Button {
-                    
-                } label: {
+                Button(action: saveButtonPressed, label: {
                     Text("Save")
                         .foregroundColor(.white)
                         .font(.headline)
@@ -29,16 +33,37 @@ struct AddItem: View {
                         .frame(maxWidth: .infinity)
                         .background(Color.accentColor)
                         .cornerRadius(10)
-                }
+                })
             }
             .padding(14)
         }
         .navigationTitle("Add item 🖊️")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+    
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    private func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Your new todo item should have atleast 3 characters long!!! 😨😱😰"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    private func getAlert() -> Alert {
+        Alert(title: Text(alertTitle))
     }
 }
 
 #Preview {
     NavigationView {
         AddItem()
-    }
+    }.environmentObject(ListViewModel())
 }

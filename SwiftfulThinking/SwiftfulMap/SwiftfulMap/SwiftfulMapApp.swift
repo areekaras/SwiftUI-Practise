5baefe27eddb31280a79427e2ab10d5848dbeead
areekaras <shibili.areekara@gmail.com>
2026-09-17 09:35:38 +0530

    Initial commit - SwiftfulMap

---
+++ SwiftfulThinking/SwiftfulMap/SwiftfulMap/SwiftfulMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapApp.swift
+//  SwiftfulMap
+//
+//  Created by Shibili Areekara on 17/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

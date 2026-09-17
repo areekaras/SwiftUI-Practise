71489380007207603d9727357776444737712df0
areekaras <shibili.areekara@gmail.com>
2026-09-17 10:23:09 +0530

    Add map to the LocationsView

--- SwiftfulThinking/SwiftfulMap/SwiftfulMap/ViewModels/LocationsViewModel.swift
+++ SwiftfulThinking/SwiftfulMap/SwiftfulMap/ViewModels/LocationsViewModel.swift
@@ -5,13 +5,35 @@
 //  Created by Shibili Areekara on 17/09/26.
 //
 
+import SwiftUI
 import Combine
+import MapKit
 
 class LocationsViewModel: ObservableObject {
     
     @Published var locations: [Location]
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(for: mapLocation)
+        }
+    }
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    private let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
-        self.locations = LocationsDataService.locations
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        
+        updateMapRegion(for: self.mapLocation)
+    }
+    
+    private func updateMapRegion(for location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
     }
 }

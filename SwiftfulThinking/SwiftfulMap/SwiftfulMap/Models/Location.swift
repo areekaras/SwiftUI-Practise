39622b674d918febb4d05097c416f1ba3e800417
areekaras <shibili.areekara@gmail.com>
2026-09-17 09:43:58 +0530

    Add project set up files

---
+++ SwiftfulThinking/SwiftfulMap/SwiftfulMap/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftfulMap
+//
+//  Created by Shibili Areekara on 17/09/26.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}

718e99684de594c2cbc7516e173c45a12bbf9b70
areekaras <shibili.areekara@gmail.com>
2026-09-19 13:02:01 +0530

    Load and show business on the UI from core data

--- SwiftfulThinking/CoreDataBootcamp/CoreDataBootcamp/RelationshipBootcamp/RelationshipBootcamp.swift
+++ SwiftfulThinking/CoreDataBootcamp/CoreDataBootcamp/RelationshipBootcamp/RelationshipBootcamp.swift
@@ -43,14 +43,43 @@ class RelationshipViewModel: ObservableObject {
     
     @Published var businesses: [Business] = []
     
+    init() {
+        getBusinesses()
+    }
+    
+    func getBusinesses() {
+        let request = NSFetchRequest<Business>(entityName: "Business")
+        
+        do {
+            businesses = try manager.context.fetch(request)
+        } catch {
+            print("Fetch error \(error)")
+        }
+    }
+    
     func addBusiness() {
         let newBusiness = Business(context: manager.context)
         newBusiness.name = "Apple"
+        
+        // Add existing deparment to the new business
+        // newBusiness.departments = []
+        
+        // Add existing employees to the new business
+        // newBusiness.employees = []
+        
+        // Add new business to the existing department
+        // newBusiness.addToDepartments(<#T##value: Department##Department#>)
+        
+        // Add new employees to the existing employees
+        // newBusiness.addToEmployees(<#T##value: Employee##Employee#>)
+        
+        
         save()
     }
     
     func save() {
         manager.save()
+        getBusinesses()
     }
     
 }
@@ -62,22 +91,65 @@ struct RelationshipBootcamp: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                Button {
-                    vm.addBusiness()
-                } label: {
-                    Text("Perform Action")
-                        .font(.headline)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(height: 55)
-                        .frame(maxWidth: .infinity)
-                        .background(Color(#colorLiteral(red: 0.02008849755, green: 0.198356837, blue: 1, alpha: 1)))
-                        .cornerRadius(10)
+                VStack(spacing: 16) {
+                    Button {
+                        vm.addBusiness()
+                    } label: {
+                        Text("Perform Action")
+                            .font(.headline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(#colorLiteral(red: 0.02008849755, green: 0.198356837, blue: 1, alpha: 1)))
+                            .cornerRadius(10)
+                    }
+                    
+                    ScrollView(.horizontal, showsIndicators: true) {
+                        HStack(alignment: .top) {
+                            ForEach(vm.businesses) { business in
+                                BusinessView(business: business)
+                            }
+                        }
+                    }
                 }
                 .padding()
             }
-            .navigationTitle("Business")
+            .navigationTitle("Relationships")
         }
+    }
+}
+
+struct BusinessView: View {
+    var business: Business
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Name: \(business.name ?? "")")
+                .bold()
+                
+            if let departments = business.departments?.allObjects as? [Department] {
+                Text("Departments:")
+                    .bold()
+                
+                ForEach(departments) { department in
+                    Text(department.name ?? "")
+                }
+            }
+            
+            if let employees = business.employees?.allObjects as? [Employee] {
+                Text("Employees:")
+                    .bold()
+                ForEach(employees) { employee in
+                    Text(employee.name ?? "")
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300, alignment: .leading)
+        .background(Color.gray.opacity(0.5))
+        .cornerRadius(10)
+        .shadow(radius: 10)
     }
 }
 

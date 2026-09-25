3cedb4039b5e058e53d47af091cc460a8c397f1f
areekaras <shibili.areekara@gmail.com>
2026-09-25 11:22:12 +0530

    Add animation for header button click

--- SwiftfulThinking/SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulThinking/SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -8,27 +8,49 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
         ZStack {
+            // background layer
             Color.theme.background
                 .ignoresSafeArea()
             
+            // content layer
             VStack {
-                HStack {
-                    CircleButtonView(iconName: "info")
-                    Spacer()
-                    Text("Live Prices")
-                        .font(.headline)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.theme.accent)
-                    Spacer()
-                    CircleButtonView(iconName: "chevron.right")
-                }
-                .padding(.horizontal)
+                homeHeader
                 
                 Spacer(minLength: 0)
             }
         }
+    }
+}
+
+extension HomeView {
+    var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus": "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
 

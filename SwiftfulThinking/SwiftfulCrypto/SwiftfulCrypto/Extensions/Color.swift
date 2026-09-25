7ea9f615a715acdc2e8787ccc1f7a68dd932b93b
areekaras <shibili.areekara@gmail.com>
2026-09-25 10:57:07 +0530

    Rename color assets name to avoid name conflict warning

---
+++ SwiftfulThinking/SwiftfulCrypto/SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Shibili Areekara on 25/09/26.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenAppColor")
+    let red = Color("RedAppColor")
+    let secondary = Color("SecondaryTextColor")
+}

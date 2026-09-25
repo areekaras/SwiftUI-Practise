d377aaee581caa7aabba672d2fa759364fd3bac8
areekaras <shibili.areekara@gmail.com>
2026-09-25 10:09:31 +0530

    Initial Commit with Project set up

---
+++ SwiftfulThinking/SwiftfulCrypto/SwiftfulCrypto/Extension/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Shibili Areekara on 25/09/26.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    static let accent = Color("AccentColor")
+    static let background = Color("BackgroundColor")
+    static let green = Color("GreenColor")
+    static let red = Color("RedColor")
+    static let secondary = Color("SecondaryTextColor")
+}
---
+++ SwiftfulThinking/SwiftfulCrypto/SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Shibili Areekara on 25/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
